4b7a70fd764f310e8f214c65e25f6281175077ab
matiaswasiak <matiaswasiak@gmail.com>
2021-01-16 15:26:23 -0300

    Getters y Setters

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,16 @@
-import MediaPlayer from "./MediaPlayer.js";
-import AutoPlay from "./plugins/AutoPlay.js";
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
-const video = document.querySelector("video");
+const video = document.querySelector('video');
 const player = new MediaPlayer({
   el: video,
-  plugins: [
-    //new AutoPlay()
-  ],
+  plugins: [new AutoPlay()],
 });
 
-const button = document.querySelector("button");
-button.onclick = () => player.togglePlay();
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
 
-const muteButton = document.querySelector("#muteButton");
+const muteButton = document.querySelector('#muteButton');
 muteButton.onclick = () => {
   if (player.media.muted) {
     player.unmute();
