ff99054415aa6398fd6ab84f212d602c814b6d3d
matiaswasiak <matiaswasiak@gmail.com>
2021-01-18 22:32:31 -0300

    IntersectionObserver

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,17 @@
-import MediaPlayer from './MediaPlayer.js';
-import AutoPlay from './plugins/AutoPlay.js';
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
 
-const video = document.querySelector('video');
+const video = document.querySelector("video");
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay()],
+  plugins: [new AutoPlay(), new AutoPause()],
 });
 
-const playButton = document.querySelector('#playButton');
+const playButton = document.querySelector("#playButton");
 playButton.onclick = () => player.togglePlay();
 
-const muteButton = document.querySelector('#muteButton');
+const muteButton = document.querySelector("#muteButton");
 muteButton.onclick = () => {
   if (player.media.muted) {
     player.unmute();
