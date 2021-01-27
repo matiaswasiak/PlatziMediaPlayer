e991091eb2946a6c42bd7167e3a93d818ba88b7a
matiaswasiak <matiaswasiak@gmail.com>
2021-01-27 16:58:54 -0300

    Tipos Básicos

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,7 +1,40 @@
-console.log("Hello, TypeScript");
+// Boolean
+let muted: boolean = true;
+muted = false;
 
-function add(a: number, b: number) {
-  return a + b;
+// Números
+let age = 6;
+let numerador: number = 42;
+let denominador: number = 6;
+let resultado = numerador / denominador;
+
+// String
+let nombre: string = "Matias";
+let saludo = `Me llamo &{nombre}`;
+
+// Arreglos
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Raul"];
+// people.push("9000");
+
+let peopleAndNumber: Array<string | number> = [];
+peopleAndNumber.push("Matias");
+peopleAndNumber.push(9000);
+
+// Enum
+enum Color {
+  Rojo = "Rojo",
+  Verde = "Verde",
+  Azul = "Azul",
+  Amarillo = "Amarillo",
 }
 
-const sum = add(2, 3);
+let colorFavorito: Color = Color.Verde;
+console.log(`Mi color favorito es ${colorFavorito}`);
+
+// Any
+let comodin: any = "Joker";
+comodin = { type: "Wildcard" };
+
+// Object
+let someObject: object = { type: "Wildcard" };
