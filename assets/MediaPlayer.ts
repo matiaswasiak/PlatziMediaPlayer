f8ca480871f5d5a18aa2591df2dafed27cb9632a
matiaswasiak <matiaswasiak@gmail.com>
2021-02-03 20:58:47 -0300

    Convertir el proyecto a TypeScript

--- assets/MediaPlayer.ts
+++ assets/MediaPlayer.ts
@@ -1,31 +1,26 @@
 class MediaPlayer {
+  media: HTMLMediaElement;
+  plugins: Array<any>;
+
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [];
-    this._initPlugins();
+    this.initPlugins();
   }
-  _initPlugins() {
-    const player = {
-      play: () => this.play(),
-      pause: () => this.pause(),
-      media: this.media,
-      get muted() {
-        return this.media.muted;
-      },
-      set muted(value) {
-        this.media.muted = value;
-      },
-    };
-    this.plugins.forEach(plugin => {
-      plugin.run(player);
+  private initPlugins() {
+    this.plugins.forEach((plugin) => {
+      plugin.run(this);
     });
   }
+
   play() {
     this.media.play();
   }
+
   pause() {
     this.media.pause();
   }
+
   togglePlay() {
     if (this.media.paused) {
       this.play();
@@ -33,9 +28,11 @@ class MediaPlayer {
       this.pause();
     }
   }
+
   mute() {
     this.media.muted = true;
   }
+
   unmute() {
     this.media.muted = false;
   }
