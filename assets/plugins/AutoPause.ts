a5abf891207314be95cd7b5b46162ab8db489019
matiaswasiak <matiaswasiak@gmail.com>
2021-02-03 20:40:51 -0300

    Clases

--- assets/plugins/AutoPause.ts
+++ assets/plugins/AutoPause.ts
@@ -1,22 +1,28 @@
+import MediaPlayer from '../MediaPlayer';
+
 class AutoPause {
+  private threshold: number;
+  player: MediaPlayer;
+
   constructor() {
     this.threshold = 0.25;
-    this.handlerIntersection = this.handlerIntersection.bind(this);
+    this.handleIntersection = this.handleIntersection.bind(this);
     this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
+
   run(player) {
     this.player = player;
 
-    const observer = new IntersectionObserver(this.handlerIntersection, {
+    const observer = new IntersectionObserver(this.handleIntersection, {
       threshold: this.threshold,
     });
 
     observer.observe(this.player.media);
 
-    document.addEventListener("visibilitychange", this.handleVisibilityChange);
+    document.addEventListener('visibilitychange', this.handleVisibilityChange);
   }
 
-  handlerIntersection(entries) {
+  private handleIntersection(entries: IntersectionObserverEntry[]) {
     const entry = entries[0];
 
     const isVisible = entry.intersectionRatio >= this.threshold;
@@ -28,8 +34,8 @@ class AutoPause {
     }
   }
 
-  handleVisibilityChange() {
-    const isVisible = document.visibilityState === "visible";
+  private handleVisibilityChange() {
+    const isVisible = document.visibilityState === 'visible';
     if (isVisible) {
       this.player.play();
     } else {
