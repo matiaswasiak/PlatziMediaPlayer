caac7b7ce2c15236a96f682596161e53e0f85794
matiaswasiak <matiaswasiak@gmail.com>
2021-02-06 19:24:03 -0300

    Implementación de plugin de Ads: Desplegando en pantalla

--- assets/index.ts
+++ assets/index.ts
@@ -1,18 +1,18 @@
-import MediaPlayer from "./MediaPlayer";
-import AutoPlay from "./plugins/AutoPlay";
-import AutoPause from "./plugins/AutoPause";
-import Ads from "./plugins/Ads";
+import MediaPlayer from './MediaPlayer';
+import AutoPlay from './plugins/AutoPlay';
+import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
-const video = document.querySelector("video");
+const video = document.querySelector('video');
 const player = new MediaPlayer({
   el: video,
   plugins: [new AutoPlay(), new AutoPause(), new Ads()],
 });
 
-const playButton: HTMLElement = document.querySelector("#playButton");
+const playButton: HTMLElement = document.querySelector('#playButton');
 playButton.onclick = () => player.togglePlay();
 
-const muteButton: HTMLElement = document.querySelector("#muteButton");
+const muteButton: HTMLElement = document.querySelector('#muteButton');
 muteButton.onclick = () => {
   if (player.media.muted) {
     player.unmute();
@@ -21,8 +21,8 @@ muteButton.onclick = () => {
   }
 };
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js").catch((error) => {
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
     console.log(error.message);
   });
 }
